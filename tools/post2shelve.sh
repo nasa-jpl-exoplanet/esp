da657355d4c32fe9be8d84eacdb1a34eed8657bc
groudier <gael.m.roudier@jpl.nasa.gov>
2025-03-17 16:33:35 -0700

    post2shelve.sh rebased properly

--- tools/post2shelve.sh
+++ tools/post2shelve.sh
@@ -9,14 +9,8 @@ export DAWGIE_DB_HOST=excalibur.jpl.nasa.gov \
        EXCALIBUR_UID=${UID} \
        EXCALIBUR_USER=${USER} \
        EXCALIBUR_SOURCE_PATH=${root}
-<<<<<<< HEAD
-docker compose -f ${root}/.docker/compose.yaml run --remove-orphans tools \
-       dawgie.db.tools.post2shelve -O /proj/db -p ${1:-${USER:-undefined}}
-=======
 docker compose \
        --file ${root}/.docker/compose.yaml \
        --project-name ${USER,,} \
        run tools \
-          dawgie.db.tools.post2shelve -O /proj/db -p ${1:-${USER:-undefined}}
->>>>>>> 91a1722 (19: add project name to allow more than one pipeline per host (#21))
-       
+          dawgie.db.tools.post2shelve -O /proj/db -p ${1:-${USER:-undefined}}       
