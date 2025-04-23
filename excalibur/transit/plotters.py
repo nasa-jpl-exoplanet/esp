d9acd0e17053e57b017dcca188d5a495915ae8a2
bryden <bryden@jpl.nasa.gov>
2025-04-23 10:18:33 -0700

    cleaned up corner for whitelight

--- excalibur/transit/plotters.py
+++ excalibur/transit/plotters.py
@@ -24,119 +24,97 @@ log = logging.getLogger(__name__)
 
 
 def plot_corner(
-    # allkeys,
     alltraces,
-    # modelParams_bestFit,
     prior_ranges,
     p,
-    saveDir=os.path.join(excalibur.context['data_dir'], 'bryden/'),
+    saveDir=os.path.join(excalibur.context['data_dir'], 'debug/'),
     savetodisk=False,
 ):
     '''corner plot showing posterior distributions (transit, not cerberus)'''
 
     fitcolor = 'firebrick'
 
-    allkeys = []
+    fit_param_names = []
     modelParams_bestFit = []
+    chainlen = 0
+    numwalkers = 0
     for key, values in alltraces.items():
-        allkeys.append(key)
+        fit_param_names.append(key)
         modelParams_bestFit.append(np.nanmedian(values))
-        print(
-            'WHITELIGHT trace median,std,min,max',
-            key,
-            np.nanmedian(values),
-            np.nanstd(values),
-            np.nanmin(values),
-            np.nanmax(values),
-        )
+        chainlen = max(chainlen, len(values[0]))
+        numwalkers = max(numwalkers, len(values[:, 0]))
+        # print(
+        #    'WHITELIGHT trace median,std,min,max',
+        #    key,
+        #    np.nanmedian(values),
+        #    np.nanstd(values),
+        #    np.nanmin(values),
+        #    np.nanmax(values),
+        # )
+    # print('chainlen,numwalkers', chainlen, numwalkers)
 
-    print(' params inside of corner plotting', allkeys)
+    # print('mctrace params', alltraces.keys())
+    # print(' params inside of corner plotting', fit_param_names)
+    # place rprs as the first parameters? asdf
 
-    print('mctrace params', alltraces.keys())
+    # print('bestfit values passed in (currently median)', modelParams_bestFit)
+    # print()
 
-    print('bestfit values passed in (currently median)', modelParams_bestFit)
-    print()
-
-    lo = []
-    hi = []
-    priorlo = []
-    priorhi = []
-    mcmcMedian = []
-    # awkward because alltraces is a dictionary
-    #  convert to 2-d array first (needed for corner() anyway)
-    paramnames = []
+    #  convert to the tracearray to a 2-d array (corner() format)
     tracearray = []
     for param, values in alltraces.items():
-        paramnames.append(param)
-        tracearray.append(values)
-        print('shape added', param, values.shape)
-    print('tracearray', tracearray)
+        # fixed parameters only have 2 steps. extend them
+        if len(values[0]) < chainlen:
+            # print('EXTENDING A FIXED PARAM', param, numwalkers, chainlen)
+            tracearray.append(
+                float(values[0][0]) * np.ones((numwalkers, chainlen))
+            )
+        else:
+            tracearray.append(np.array(values))
     tracearray = np.array(tracearray)
 
-    for param, values in alltraces.items():
-        mcmcMedian.append(np.nanmedian(values))
-        lo.append(np.nanpercentile(np.array(values), 16))
-        hi.append(np.nanpercentile(np.array(values), 84))
-        # span = hi - lo
-        # Careful! these are not actually the prior ranges;
-        #  they're the range of walker values (unless set below)
-        priorlo.append(np.nanmin(values))
-        priorhi.append(np.nanmax(values))
-    print('medians inside of corner plotting', mcmcMedian)
-
-    mcmcMedian = np.nanmedian(tracearray, axis=1)
-    lo = np.nanpercentile(np.array(tracearray), 16, axis=1)
-    hi = np.nanpercentile(np.array(tracearray), 84, axis=1)
-    # span = hi - lo
+    mcmcMedian = np.nanmedian(tracearray, axis=(1, 2))
     # Careful! these are not actually the prior ranges;
     #  they're the range of walker values (unless set below)
-    priorlo = np.nanmin(tracearray, axis=1)
-    priorhi = np.nanmax(tracearray, axis=1)
-    print('medians inside of corner plotting', mcmcMedian)
-    print()
-    print()
+    priorlo = np.nanmin(tracearray, axis=(1, 2))
+    priorhi = np.nanmax(tracearray, axis=(1, 2))
+    # print('medians inside of corner plotting', mcmcMedian)
+    # print('  priorlo', priorlo)
 
-    for ikey, key in enumerate(allkeys):
-        print('param:', ikey, key)
-        print('  old prior range:', key, priorlo[ikey], priorhi[ikey])
+    # for cases with fixed params, make sure the plots have some range
+    #  actually this is not needed. corner() can handle it
+
+    for ikey, key in enumerate(fit_param_names):
         if key in prior_ranges.keys():
+            # print(
+            #    '  old prior range (median):', key, priorlo[ikey], priorhi[ikey]
+            # )
             priorlo[ikey] = prior_ranges[key][0]
             priorhi[ikey] = prior_ranges[key][1]
-        else:
-            print('  TROUBLE: ', key, 'not found in', prior_ranges.keys())
-        print('  new prior range:', key, priorlo[ikey], priorhi[ikey])
-    # priorspan = priorhi - priorlo
-    # priormid = (priorhi + priorlo) / 2.
-
-    lodist = np.array(mcmcMedian) - np.array(lo)
-    hidist = np.array(hi) - np.array(mcmcMedian)
-    lorange = np.array(mcmcMedian) - 3 * lodist
-    hirange = np.array(mcmcMedian) + 3 * hidist
-    trange = [tuple([x, y]) for x, y in zip(lorange, hirange)]
-    # previous lines did 3-sigma range.  better to just use the prior bounds as bounds
+            # print(
+            #    '  new prior range (true)  :', key, priorlo[ikey], priorhi[ikey]
+            # )
     trange = [tuple([x, y]) for x, y in zip(priorlo, priorhi)]
-    # print('trange',trange)
-    # print('lodist',lodist)
-    # print('lorange',lorange)
+
     figure = corner.corner(
-        np.vstack(np.array(tracearray)).T,
+        np.vstack(np.array(tracearray).T),
         bins=10,
-        labels=allkeys,
+        labels=fit_param_names,
         range=trange,
         show_titles=True,
         quantiles=[0.16, 0.50, 0.84],
     )
 
-    ndim = len(allkeys)
+    ndim = len(fit_param_names)
     axes = np.array(figure.axes).reshape((ndim, ndim))
     # use larger font size for the axis labels
     for i in range(ndim):
         ax = axes[ndim - 1, i]
-        ax.set_xlabel(allkeys[i], fontsize=14)
+        ax.set_xlabel(fit_param_names[i], fontsize=14)
     for i in range(ndim - 1):
         # skipping the first one on the y side (it's a histo, not a 2-D plot)
         ax = axes[i + 1, 0]
-        ax.set_ylabel(allkeys[i + 1], fontsize=14)
+        ax.set_ylabel(fit_param_names[i + 1], fontsize=14)
     #  draw a point and crosshair for the medians in each subpanel
     for yi in range(ndim):
         for xi in range(yi):
@@ -159,6 +137,8 @@ def plot_corner(
         ax.axvline(modelParams_bestFit[i], color=fitcolor, lw=2, zorder=12)
 
     if savetodisk:
+        saveDir = '/proj/sdp/data/debug/'
+        # print('  Saving figure to disk!',saveDir+'corner-'+p+'.png')
         plt.savefig(saveDir + 'corner-' + p + '.png')
 
     return save_plot_tosv(figure), figure
