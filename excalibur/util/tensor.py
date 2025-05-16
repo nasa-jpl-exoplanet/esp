ca0fe1699075892a31f5be82d6c61fa48120f259
bryden <bryden@jpl.nasa.gov>
2025-05-15 17:42:20 -0700

    pylint fix

--- excalibur/util/tensor.py
+++ excalibur/util/tensor.py
@@ -1,10 +1,39 @@
 '''cerberus bot ds'''
 
+# pylint: disable=abstract-method,arguments-differ
+
 # -- IMPORTS -- ------------------------------------------------------
+import numpy as np
+
 import pytensor.graph as tnsrgraph
 import pytensor.tensor as tnsr
 
-class TensorShell(tnsrgraph.Op):
+
+# GMR: Gregoire s legacy
+def LogLikelihood(inputs):
+    '''
+    GMR: User defined loglikelihood
+    We stick to the proper definition of it
+    '''
+    newnodes = []
+    newindex = 0
+    for ns in ctxt.nodeshape:
+        if ns > 1:
+            newnodes.append(inputs[newindex : newindex + ns])
+            pass
+        else:
+            newnodes.append(inputs[newindex])
+            pass
+        newindex += ns
+        pass
+    ForwardModel = orbital(*newnodes)
+    # Norm = np.log(np.sqrt(2e0 * np.pi)) - np.log(ctxt.mcmcsig)
+    Norm = np.log(2e0 * np.pi * ctxt.mcmcsig)
+    out = -(((ctxt.mcmcdat - ForwardModel) / ctxt.mcmcsig) ** 2) / 2e0 - Norm
+
+    return out
+
+class TensorShell(LogLikelihood, tnsrgraph.Op):
     '''
     GMR: Tensor Shell for custom models
     Do not touch the name of the methods
