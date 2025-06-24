80bc20ad7f4b680d28f2fbff5cf428dbd27ef064
bryden <bryden@jpl.nasa.gov>
2025-06-23 22:46:38 -0700

    include analysis/agent in __init__, for analysis

--- excalibur/testcerb/__init__.py
+++ excalibur/testcerb/__init__.py
@@ -10,6 +10,11 @@ import excalibur.testcerb.bot as testcerbbot
 DAWGIE_IGNORE = False
 
 
+def analysis(prefix: str, ps_hint: int = 0, runid: int = -1):
+    '''analysis ds'''
+    return testcerbbot.Agent(prefix, ps_hint, runid)
+
+
 def task(
     prefix: str, ps_hint: int = 0, runid: int = -1, target: str = '__none__'
 ):
