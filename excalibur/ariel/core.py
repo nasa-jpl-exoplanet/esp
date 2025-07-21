40032d5e0b76413406949b10f6b30333f3b390d7
bryden <bryden@jpl.nasa.gov>
2025-07-20 17:47:59 -0700

    autoformat

--- excalibur/ariel/core.py
+++ excalibur/ariel/core.py
@@ -63,7 +63,9 @@ def calc_mmw_Hs(pressureArray, temperature, logg, X2Hr=0, useTEA=False):
     calculate the mean molecular weight and scale height
     '''
     if useTEA:
-        mixratio, fH2, fHe = crbutil.calcTEA(pressureArray, temperature, X2Hr=X2Hr)
+        mixratio, fH2, fHe = crbutil.calcTEA(
+            pressureArray, temperature, X2Hr=X2Hr
+        )
     else:
         mixratio, fH2, fHe = crbutil.crbce(
             pressureArray, temperature, X2Hr=X2Hr
