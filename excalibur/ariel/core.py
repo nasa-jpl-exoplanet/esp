8cfa848ec26a63e07158e60fda7825ee3b50ca02
bryden <bryden@jpl.nasa.gov>
2025-08-06 13:36:44 -0700

    diff T-P profile params

--- excalibur/ariel/core.py
+++ excalibur/ariel/core.py
@@ -66,7 +66,8 @@ def calc_mmw_Hs(pressureArray, temperature, logg, X2Hr=0, useTEA=False):
     calculate the mean molecular weight and scale height
     '''
     if useTEA:
-        tempCoeffs = [0, temperature, 0, 0, 0, 0, 0, 0, 0, 0]
+        # tempCoeffs = [0, temperature, 0, 0, 0, 0, 0, 0, 0, 0]
+        tempCoeffs = [0, temperature]
         species = ['H2O', 'CO', 'CO2']
         mixratio, fH2, fHe = crbutil.calcTEA(
             tempCoeffs, pressureArray, species, metallicity=10.0**X2Hr
