60786c5c9003aee5b448ccd62809367ab6ba79dd
Al Niessner <Al.Niessner@xxx.xxx>
2025-08-08 11:05:09 -0700

    add runs at boot

--- excalibur/metrics/__init__.py
+++ excalibur/metrics/__init__.py
@@ -6,7 +6,13 @@ import excalibur.metrics.bot
 # ------------- ------------------------------------------------------
 DAWGIE_IGNORE = False
 
+import dawgie
+
+from importlib import import_module as fetch  # avoid cyclic-import
 
 def analysis(prefix: str, ps_hint: int = 0, runid: int = -1):
     '''metrics are global or an aspect'''
     return excalibur.metrics.bot.AnalysisTeam(prefix, ps_hint, runid)
+
+def events():
+    return [dawgie.schedule(analysis, fetch('excalibur.metrics.algorithms').Performance(), boot=True)
