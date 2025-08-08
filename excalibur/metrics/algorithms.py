a055f7486b8f4efbf3373a94321f15a3249d02d3
Al Niessner <Al.Niessner@xxx.xxx>
2025-08-08 12:01:25 -0700

    style fix

--- excalibur/metrics/algorithms.py
+++ excalibur/metrics/algorithms.py
@@ -15,26 +15,26 @@ class Performance(dawgie.Analyzer):
 
     def __init__(self):
         '''init the performance process'''
-        self._version_ = dawgie.VERSION(1,0,0)
-        self._svs = [states.CpuAndMem('test',[])]
+        self._version_ = dawgie.VERSION(1, 0, 0)
+        self._svs = [states.CpuAndMem('test', [])]
 
-    def name(self)->str:
+    def name(self) -> str:
         '''database name'''
         return 'performance'
 
-    def run(self, aspects: dawgie.Aspect)->None:
+    def run(self, aspects: dawgie.Aspect) -> None:
         '''load dawgie.db.metrics() then process it to states.CpuAndMem'''
         metrics = dawgie.db.metrics()
         table = {}
         for md in metrics:
-            name = '.'.join([md.task,md.alg_name])
+            name = '.'.join([md.task, md.alg_name])
             known = table.get(name, [])
             known.append(md)
             table[name] = known
-        self._svs = [states.CpuAndMem(**i) for i in known.items()] 
+        self._svs = [states.CpuAndMem(**i) for i in known.items()]
         aspects.ds().update()
 
-    def state_vectors(self)->[dawgie.StateVector]:
+    def state_vectors(self) -> [dawgie.StateVector]:
         '''products of this aspect that is fully dynamically created'''
         return self._svs
 
@@ -45,4 +45,3 @@ class Performance(dawgie.Analyzer):
     def where(self):
         '''this task should always run on the local cluster'''
         return dawgie.Distribution.cluster
-
--- excalibur/metrics/states.py
+++ excalibur/metrics/states.py
@@ -16,14 +16,16 @@ class CpuAndMem(dawgie.StateVector):
 
     def __init__(self, name, mds):
         '''init the state vector by building it from condensed form'''
-        self._version_ = dawgie.VERSION(1,0,0)
+        self._version_ = dawgie.VERSION(1, 0, 0)
         self._name = name
         self['byrid'] = excalibur.ValuesDict()
         self['bytn'] = excalibur.ValuesDict()
         for md in mds:
-            if md.run_id not in self['byrid']: sel['byrid'][md.run_id] = []
+            if md.run_id not in self['byrid']:
+                sel['byrid'][md.run_id] = []
             self['byrid'][md.run_id].append(md.sv)
-            if md.target not in self['bytn']: self['bytn'][md.target] = []
+            if md.target not in self['bytn']:
+                self['bytn'][md.target] = []
             self['bytn'][md.target].append(md.sv)
 
     def features(self):
@@ -32,60 +34,94 @@ class CpuAndMem(dawgie.StateVector):
 
     def name(self):
         '''database name'''
-        return self._name.replace('.','::')
+        return self._name.replace('.', '::')
 
     def view(self, caller: excalibur.Identity, visitor: dawgie.Visitor) -> None:
         '''Show the configutation information'''
         visitor.add_declaration_inline('', div='<div><hr>')
         visitor.add_declaration_inline('Dark blue dots are the median')
         visitor.add_declaration_inline('Thin cyan lines are the min/max')
-        visitor.add_declaration_inline('When x-axis is run id, then min/max is over targets and run id when x-axis is target')
+        visitor.add_declaration_inline(
+            'When x-axis is run id, then min/max is over targets and run id when x-axis is target'
+        )
         visitor.add_declaration_inline('', div='</div>')
-        c1r1 = plot_resource(self['byrid'], sorted(self['byrid'].keys()),
-                             self._name, 'Run ID', True)
-        c1r2 = plot_resource(self['byrid'], sorted(self['byrid'].keys()),
-                             self._name, 'Run ID', False)
+        c1r1 = plot_resource(
+            self['byrid'],
+            sorted(self['byrid'].keys()),
+            self._name,
+            'Run ID',
+            True,
+        )
+        c1r2 = plot_resource(
+            self['byrid'],
+            sorted(self['byrid'].keys()),
+            self._name,
+            'Run ID',
+            False,
+        )
         c1r2.x_range = c1r1.x_range
-        c2r1 = plot_resource(self['bytn'], sorted(self['bytn'].keys()),
-                             self._name, 'Target Name', True)
-        c2r2 = plot_resource(self['bytn'], sorted(self['bytn'].keys()),
-                             self._name, 'Target Name', False)
+        c2r1 = plot_resource(
+            self['bytn'],
+            sorted(self['bytn'].keys()),
+            self._name,
+            'Target Name',
+            True,
+        )
+        c2r2 = plot_resource(
+            self['bytn'],
+            sorted(self['bytn'].keys()),
+            self._name,
+            'Target Name',
+            False,
+        )
         c2r2.x_range = c2r1.x_range
         c2r1.y_range = c1r1.y_range
         c2r2.y_range = c1r2.y_range
-        f = bokeh.layouts.gridplot([[c1r1, c2r1],[c1r2, c2r2]],
-                                   toolbar_location='right')
-        js,div = bokeh.embed.components(f)
+        f = bokeh.layouts.gridplot(
+            [[c1r1, c2r1], [c1r2, c2r2]], toolbar_location='right'
+        )
+        js, div = bokeh.embed.components(f)
         visitor.add_declaration(None, div=div, js=js)
 
-def plot_resource (data_table, keys, tan, xlabel, mem):
+
+def plot_resource(data_table, keys, tan, xlabel, mem):
     res = 'Memory' if mem else 'Processing Time'
     svs = [data_table[k] for k in keys]
-    fig = bokeh.plotting.figure (height=400,
-                                 title=f'{res}: AE component {tan}',
-                                 tools='pan,ywheel_zoom,box_zoom,xwheel_zoom,save,reset',
-                                 width=600)
+    fig = bokeh.plotting.figure(
+        height=400,
+        title=f'{res}: AE component {tan}',
+        tools='pan,ywheel_zoom,box_zoom,xwheel_zoom,save,reset',
+        width=600,
+    )
     x = [i for i in range(len(keys))]
     y = []
     yn = []
     yx = []
     for svl in svs:
-        w = [(sv['db_memory' if mem else 'task_wall'].value() + 
-              sv['task_memory' if mem else 'task_wall'].value())*(1024 if mem else 1)
-             for sv in svl]
+        w = [
+            (
+                sv['db_memory' if mem else 'task_wall'].value()
+                + sv['task_memory' if mem else 'task_wall'].value()
+            )
+            * (1024 if mem else 1)
+            for sv in svl
+        ]
         y.append(numpy.nanmedian([numpy.nan if a < 0 else a for a in w]))
         yn.append(min(w))
         yx.append(max(w))
-    fig.segment(x,yn,x,yx,color='cyan')
-    fig.scatter(x,y,marker='*',color='blue')
+    fig.segment(x, yn, x, yx, color='cyan')
+    fig.scatter(x, y, marker='*', color='blue')
     fig.xaxis.axis_label = xlabel
-    fig.xaxis.formatter = bokeh.models.CustomJSTickFormatter(code=f'''
+    fig.xaxis.formatter = bokeh.models.CustomJSTickFormatter(
+        code=f'''
 var labels = {keys};
-return labels[tick];''')
-    fig.xaxis.major_label_orientation = math.pi/4
+return labels[tick];'''
+    )
+    fig.xaxis.major_label_orientation = math.pi / 4
     fig.yaxis.axis_label = 'Memory [B]' if mem else 'Wall Clock [days H:M:S]'
     if mem:
-        fig.yaxis.formatter = bokeh.models.CustomJSTickFormatter(code='''
+        fig.yaxis.formatter = bokeh.models.CustomJSTickFormatter(
+            code='''
 function format_engineering(value) {
     if (value === 0) return "0";
     let sign = value < 0 ? "-" : "";
@@ -94,9 +130,11 @@ function format_engineering(value) {
     let mantissa = abs_value / Math.pow(10, exponent);
     return sign + mantissa.toFixed(2) + "e" + exponent;
 }
-return format_engineering(tick);''')
+return format_engineering(tick);'''
+        )
     else:
-        fig.yaxis.formatter = bokeh.models.CustomJSTickFormatter(code='''
+        fig.yaxis.formatter = bokeh.models.CustomJSTickFormatter(
+            code='''
 function convertSecondsToDHMS(totalSeconds) {
   totalSeconds = Number(totalSeconds);
   const days = Math.floor(totalSeconds / (24 * 3600));
@@ -112,5 +150,6 @@ function convertSecondsToDHMS(totalSeconds) {
   result += `${seconds.toString().padStart(2, '0')}`;
   return result;
 }
-return convertSecondsToDHMS(tick);''')
+return convertSecondsToDHMS(tick);'''
+        )
     return fig
