f94ae5a6cd32f82ce82d0539be2b74ce60f18870
al-niessner <1130658+al-niessner@users.noreply.github.com>
2025-10-12 14:59:02 -0700

    Update __main__.py

--- excalibur/target/__main__.py
+++ excalibur/target/__main__.py
@@ -16,17 +16,17 @@ import excalibur.target.bot
 rid, tn = main_start()
 
 if tn in ['', '__all__']:
-    NAME = ['create', None][-1]  # -1 to run them all
+    name = ['create', None][-1]  # -1 to run them all
     subtasks = excalibur.target.bot.Agent('target', 4, rid)
     pass
 elif rid == 0:
-    NAME = ['scrape_regression', None][-1]  # -1 to run them all
+    name = ['scrape_regression', None][-1]  # -1 to run them all
     subtasks = excalibur.target.bot.Regress('target', 4, tn)
 else:
-    NAME = ['autofill', 'scrape', None][-1]  # -1 to run them all
+    name = ['autofill', 'scrape', None][-1]  # -1 to run them all
     subtasks = excalibur.target.bot.Actor('target', 4, rid, tn)
     pass
 
-subtasks.do(NAME)
+subtasks.do(name)
 dawgie.db.close()
 dawgie.security.finalize()
