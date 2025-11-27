11a82b5944b4b9bce4060ba13c45a44ea134ca34
bryden <bryden@jpl.nasa.gov>
2025-11-26 16:13:55 -0800

    move ariel plotting out to new plotters.py

--- excalibur/ariel/core.py
+++ excalibur/ariel/core.py
@@ -22,14 +22,13 @@ from excalibur.ariel.clouds import fixedCloudParameters, randomCloudParameters
 from excalibur.ariel.ariel_instrument_model import load_ariel_instrument
 from excalibur.ariel.forward_models import make_cerberus_atmos
 from excalibur.cerberus.core import myxsecs
-from excalibur.util.plotters import add_scale_height_labels, save_plot_tosv
+from excalibur.ariel.plotters import plot_spectrum, plot_spectrum_topmolecules, plot_depthprobed
 
 # import os
 # import sys
 
 # import pickle
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.constants as cst
 
 log = logging.getLogger(__name__)
@@ -325,8 +324,9 @@ def simulate_spectra(
 
             # ________LOOP OVER ALL SELECTED MODELS_______
             for atmosModel in atmosModels:
-                # print()
-                # print('starting Atmospheric Model:',atmosModel)
+                if verbose:
+                    print()
+                    print('starting Atmospheric Model:',atmosModel)
                 useTEA = bool('TEA' in atmosModel)
                 if useTEA:
                     chemistry = 'TEA'
@@ -362,7 +362,7 @@ def simulate_spectra(
                         model_params['N/O'] = 0
 
                 # check whether this planet+metallicity combo is convergent/bound atmosphere
-                mmw, Hs = calc_mmw_Hs(
+                _, Hs = calc_mmw_Hs(
                     pressure,
                     eqtemp,
                     model_params['logg'],
@@ -594,175 +594,51 @@ def simulate_spectra(
                     fluxDepth_by_molecule_rebin[molecule] = (
                         fluxDepth_by_molecule_rebin[molecule] * 100
                     )
-
-                # PLOT THE SPECTRA
-                myfig, ax = plt.subplots(figsize=(8, 4))
-                plt.title(
-                    'Ariel simulation : '
-                    + target
-                    + ' '
-                    + planet_letter
-                    + ' : Tier-'
-                    + str(tier)
-                    + ' '
-                    + str(visits)
-                    + ' visits',
-                    fontsize=16,
-                )
-                plt.xlabel(str('Wavelength [$\\mu m$]'), fontsize=14)
-                plt.ylabel(str('$(R_p/R_*)^2$ [%]'), fontsize=14)
-
-                # plot the true (model) spectrum - raw
-                #  (this is for testing higher resolution cross-sections
-                #   but currently raw and rebin are the same thing)
-                # plt.plot(
-                #    wavelength_um,
-                #    fluxDepth,
-                #    color='palegoldenrod',
-                #    ls='-',
-                #    lw=1,
-                #    zorder=1,
-                #    label='truth raw',
-                # )
-                # plot the true (model) spectrum - binned
-                plt.plot(
+                out['data'][planet_letter][atmosModel]['plot_simspectrum'] = plot_spectrum(
+                    target,
+                    planet_letter,
+                    tier,
+                    visits,
                     wavelength_um_rebin,
                     fluxDepth_rebin,
-                    color='k',
-                    ls='-',
-                    lw=1,
-                    zorder=3,
-                    # label='truth binned',
-                    label='truth',
-                )
-                yrange = plt.ylim()
-                # plot the simulated data points
-                plt.scatter(
-                    wavelength_um_rebin,
                     fluxDepth_observed,
-                    marker='o',
-                    s=20,
-                    color='None',
-                    edgecolor='k',
-                    zorder=4,
-                    label='sim data',
+                    uncertainties_percent,
+                    molecules,
+                    fluxDepth_by_molecule_rebin,
+                    out['data'][planet_letter][atmosModel]['Hs'],
+                    atmosModel,
+                    verbose=verbose,
                 )
-                plt.errorbar(
+                out['data'][planet_letter][atmosModel]['plot_simspectrum_topmolecules'] = plot_spectrum_topmolecules(
+                    target,
+                    planet_letter,
+                    tier,
+                    visits,
                     wavelength_um_rebin,
+                    fluxDepth_rebin,
                     fluxDepth_observed,
-                    yerr=uncertainties_percent,
-                    linestyle='None',
-                    lw=0.2,
-                    color='grey',
-                    zorder=2,
+                    uncertainties_percent,
+                    molecules,
+                    fluxDepth_by_molecule_rebin,
+                    out['data'][planet_letter][atmosModel]['Hs'],
+                    atmosModel,
+                    verbose=verbose,
                 )
-                plt.ylim(yrange)
-
-                # calculate the baseline and total range
-                #  use this to omit molecules that are not contributing
-                baseline = 666
-                maxdepth = -666
-                for imole, molecule in enumerate(molecules):
-                    baseline = np.min(
-                        np.append(
-                            baseline, fluxDepth_by_molecule_rebin[molecule]
-                        )
-                    )
-                    maxdepth = np.max(
-                        np.append(
-                            maxdepth, fluxDepth_by_molecule_rebin[molecule]
-                        )
-                    )
-                negligible_molecules = ''
-                for imole, molecule in enumerate(molecules):
-                    colorlist = [
-                        'red',
-                        'orange',
-                        'palegreen',
-                        'lightseagreen',
-                        'blueviolet',
-                        'fuchsia',
-                    ]
-                    stylelist = [
-                        '--',
-                        '--',
-                        '--',
-                        '--',
-                        '--',
-                        '--',
-                        ':',
-                        ':',
-                        ':',
-                        ':',
-                        ':',
-                        ':',
-                        '-.',
-                        '-.',
-                        '-.',
-                        '-.',
-                        '-.',
-                        '-.',
-                    ]
-                    feature_strength = (
-                        np.max(fluxDepth_by_molecule_rebin[molecule]) - baseline
-                    ) / (maxdepth - baseline)
-                    # cut off anything less than 1% of maximum contribution
-                    if feature_strength < 0.01:
-                        # print('  dropped:', molecule, feature_strength)
-                        negligible_molecules += ' ' + molecule
-                    else:
-                        # print(' OK:', molecule, feature_strength)
-                        plt.plot(
-                            wavelength_um_rebin,
-                            fluxDepth_by_molecule_rebin[molecule],
-                            color=colorlist[imole % len(colorlist)],
-                            ls=stylelist[imole % len(stylelist)],
-                            lw=1,
-                            zorder=2,
-                            label=molecule,
-                        )
-                extra = (maxdepth - baseline) / 13
-                plt.ylim((baseline - extra, maxdepth + extra))
-                yrange = plt.ylim()
-                plt.text(
-                    6.9,
-                    yrange[0] + (yrange[1] - yrange[0]) * (-0.13),
-                    'negligible contribution:',
-                    fontsize=8,
+                out['data'][planet_letter][atmosModel]['plot_depthprobed'] = plot_depthprobed(
+                    target,
+                    planet_letter,
+                    tier,
+                    visits,
+                    wavelength_um_rebin,
+                    fluxDepth_rebin,
+                    fluxDepth_observed,
+                    uncertainties_percent,
+                    molecules,
+                    fluxDepth_by_molecule_rebin,
+                    out['data'][planet_letter][atmosModel]['Hs'],
+                    atmosModel,
+                    verbose=verbose,
                 )
-                plt.text(
-                    6.8,
-                    yrange[0] + (yrange[1] - yrange[0]) * (-0.18),
-                    negligible_molecules,
-                    fontsize=8,
-                )
-                plt.xlim(0.0, 8.0)
-                plt.legend(loc='center left', bbox_to_anchor=(1.16, 0.48))
-
-                # add a scale-height-normalized flux scale on the right axis
-                Hsscaling = out['data'][planet_letter][atmosModel]['Hs']
-                # print('H scaling for this plot (%):',Hsscaling*100)
-                add_scale_height_labels(
-                    {'Hs': [Hsscaling]}, 1.0e-2 * fluxDepth_rebin, ax, myfig
-                )
-
-                # option to save plot to disk
-                # plot_dir = (
-                #     excalibur.context['data_dir'] + '/ariel/savedplots'
-                # )
-                # if not os.path.exists(plot_dir):
-                #    os.mkdir(plot_dir)
-                # plt.savefig(plot_dir +
-                #             '/ariel_' + atmosModel + 'Atmos_' +
-                #             target + '_' + planet_letter + '.png')
-
-                out['data'][planet_letter][atmosModel]['plot_simspectrum'] = (
-                    save_plot_tosv(myfig)
-                )
-
-                if verbose:
-                    plt.show()
-                plt.close(myfig)
 
                 completed_at_least_one_planet = True
 
