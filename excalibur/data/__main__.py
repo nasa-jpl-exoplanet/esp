d89ed6e84e867ce80cb5dc623ecfe17937aa896a
groudier <gael.m.roudier@jpl.nasa.gov>
2026-03-18 16:10:56 -0700

    Oops the Oopsie

--- excalibur/data/__main__.py
+++ excalibur/data/__main__.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
             -1
         ]  # -1 to run them all
         subtasks = excalibur.data.bot.Actor('data', 4, rid, tn)
+        subtasks.do(NAME)
+        dawgie.db.close()
+        dawgie.security.finalize()
         pass
-    subtasks.do(NAME)
-    dawgie.db.close()
-    dawgie.security.finalize()
     pass
