a907156f7ca0d390bb8a63284145da7709cd6939
Sophia Grusnis <jfg72898@gmail.com>
2026-04-30 11:30:41 -0700

    My comments

--- excalibur/cerberus/__main__.py
+++ excalibur/cerberus/__main__.py
@@ -11,16 +11,18 @@ import excalibur.cerberus.bot
 
 # ------------- ------------------------------------------------------
 
-rid, tn = main_start()
+if __name__ == "__main__":
+    rid, tn = main_start()
 
-if tn in ['', '__all__']:
-    NAME = 'analysis'
-    subtasks = excalibur.cerberus.bot.Agent('cerberus', 4, rid)
-else:
-    NAME = ['atmos', 'results', 'xslib', None][-1]  # -1 to run them all
-    subtasks = excalibur.cerberus.bot.Actor('cerberus', 4, rid, tn)
+    if tn in ['', '__all__']:
+        NAME = 'analysis'
+        subtasks = excalibur.cerberus.bot.Agent('cerberus', 4, rid)
+    else:
+        NAME = ['atmos', 'results', 'xslib', None][-1]  # -1 to run them all
+        subtasks = excalibur.cerberus.bot.Actor('cerberus', 4, rid, tn)
+        pass
+
+    subtasks.do(NAME)
+    dawgie.db.close()
+    dawgie.security.finalize()
     pass
-
-subtasks.do(NAME)
-dawgie.db.close()
-dawgie.security.finalize()
