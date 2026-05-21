fcfce28f2c9d4da42f8959b09b1cfc63e37dafe4
bryden <bryden@jpl.nasa.gov>
2026-05-21 12:36:53 -0700

    use actual star props for blackbody

--- excalibur/hwo/hwo_instrument_model.py
+++ excalibur/hwo/hwo_instrument_model.py
@@ -184,70 +184,94 @@ log = logging.getLogger(__name__)
 
 #     return hwo_instrument
 
-def load_hwo_instrument(target):
+
+def load_hwo_instrument(target, system_params):
     '''
     Load in the HWO instrument response - uncertainty as a function of wavelength
 
     Uncertainty is for a single visit;
     number of observed transits is taken into account later
     '''
+
     def reso_range(start, finish, res):
         '''
         Builds a wavelength grid with specified endpoints and resolution (returns the midpoints of each bin)
         '''
         wl_low = [start]
-        res = 1. / res
+        res = 1.0 / res
         wl_high = [start + (start * res)]
         while wl_high[-1] < finish:
             wl_low.append(wl_high[-1])
-            wl_high.append(wl_low[-1] + (wl_low[-1] * res))   
+            wl_high.append(wl_low[-1] + (wl_low[-1] * res))
 
         bins = np.array([wl_low, wl_high]).T
 
         return bins
 
+    print('available system parameters:', system_params.keys())
+    Tstar = system_params['T*']
+    Rstar = system_params['R*']
+    d = system_params['dist']
+
+    planet_letter = target[-1]
+    print('available planet parameters:', system_params[planet_letter].keys())
+    transit_duration = system_params[planet_letter]['trandur']
+    print('transit duration (hours)', transit_duration)
 
     # assumed instrument parameters
-    tel_diameter = 7.2 # circumscribed diameter of EAC1
-    obsc_fact = 0.9 # correction factor for tel area, due to obscuration (if on-axis) and segemented mirrors
-    tel_area = np.pi * (tel_diameter/2)**2 * obsc_fact # telescope collecting area
+    tel_diameter = 7.2  # circumscribed diameter of EAC1
+    obsc_fact = 0.9  # correction factor for tel area, due to obscuration (if on-axis) and segemented mirrors
+    tel_area = (
+        np.pi * (tel_diameter / 2) ** 2 * obsc_fact
+    )  # telescope collecting area
 
-    wavelow = 0.2 * 1e-6 # minimum wavelength
-    wavehigh = 1.8 * 1e-6 # maximum wavelength
-    waveres = 500 # wavelength resolution
-    wavelength_bins = reso_range(wavelow,wavehigh,waveres) # wavelength grid 
-    wavelength = 0.5 * (wavelength_bins[:,0] + wavelength_bins[:,1])
-    bin_widths = wavelength_bins[:,1] - wavelength_bins[:,0]
+    wavelow = 0.2 * 1e-6  # minimum wavelength
+    wavehigh = 1.8 * 1e-6  # maximum wavelength
+    waveres = 500  # wavelength resolution
+    wavelength_bins = reso_range(wavelow, wavehigh, waveres)  # wavelength grid
+    wavelength = 0.5 * (wavelength_bins[:, 0] + wavelength_bins[:, 1])
+    bin_widths = wavelength_bins[:, 1] - wavelength_bins[:, 0]
 
-    optical_thruput = 0.626 # from 2 XeLiF and 17 Ag surfaces measured at 1 micron; based on EAC1 (no 0.5 from polarizer/dichroic, no IFS loss)
+    optical_thruput = 0.626  # from 2 XeLiF and 17 Ag surfaces measured at 1 micron; based on EAC1 (no 0.5 from polarizer/dichroic, no IFS loss)
 
     # star properties
-    BB = 2 * const.h * const.c**2 / wavelength**5 / (np.exp(const.h * const.c / (wavelength * const.k * Tstar)) - 1.0) # flux per wl per solid angle (blackbody spectral radiance)
-    F_atEarth = BB * np.pi * (Rstar / d) ** 2 # flux received at Earth
+    BB = (
+        2
+        * const.h
+        * const.c**2
+        / wavelength**5
+        / (np.exp(const.h * const.c / (wavelength * const.k * Tstar)) - 1.0)
+    )  # flux per wl per solid angle (blackbody spectral radiance)
+    F_atEarth = BB * np.pi * (Rstar / d) ** 2  # flux received at Earth
 
     # transit
-    transit_in = 3600
+    transit_in = transit_duration * 3600
     transit_out = transit_in * 1.0
 
     # collect photons
     tel_power = F_atEarth * bin_widths * tel_area * optical_thruput
     photon_energy = const.h * const.c / wavelength
     photon_rate = tel_power / photon_energy
-    photons_in_transit = photon_rate * transit_in # total photons in each bin during the transit
-    photons_out_transit = photon_rate * transit_out # total photons in each bin out of transit
+    photons_in_transit = (
+        photon_rate * transit_in
+    )  # total photons in each bin during the transit
+    photons_out_transit = (
+        photon_rate * transit_out
+    )  # total photons in each bin out of transit
 
     # noise
-    shotnoise = np.sqrt(1/photons_in_transit + 1/photons_out_transit) # shot noise per bin
+    shotnoise = np.sqrt(
+        1 / photons_in_transit + 1 / photons_out_transit
+    )  # shot noise per bin
 
-    nVisits = 1 # number of visits
-
+    nVisits = 1  # number of visits
 
     hwo_instrument = {
         'nVisits': nVisits,
         'transitDuration': transit_in,
         'wavelength': wavelength * 1e6,
-        'wavelow': wavelength_bins[:,0] * 1e6,
-        'wavehigh': wavelength_bins[:,1] * 1e6,
+        'wavelow': wavelength_bins[:, 0] * 1e6,
+        'wavehigh': wavelength_bins[:, 1] * 1e6,
         'noise': shotnoise,
     }
 
