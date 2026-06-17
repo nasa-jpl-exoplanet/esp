4e22874c86f1a026b9ab16a526723af569bfa1f3
Constant Bernardin <bernardin.constant7@gmail.com>
2026-06-17 09:25:39 -0700

    pull request corrections

--- excalibur/transit/lethe.py
+++ excalibur/transit/lethe.py
@@ -14,15 +14,31 @@ def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
     '''
 
     # Parameters generation
-    z_1 = np.arange(0.0, 0.8, step_z_1)
-    z_2 = np.arange(0.8, 1.2 + step_z_2, step_z_2)
-    z_grid = np.concatenate((z_1, z_2))
+    z_grid = np.concatenate(
+        (
+            np.arange(0.0, 0.8, step_z_1),
+            np.arange(0.8, 1.2 + step_z_2, step_z_2),
+        )
+    )
     rprs_grid = np.arange(0.0, 0.2 + step_rprs, step_rprs)
 
+    # powers of the LD model
+    alpha_grid = np.array(
+        [
+            1.0 / 4.0,
+            2.0 / 4.0,
+            3.0 / 4.0,
+            4.0 / 4.0,
+            2.0 / 4.0,
+            4.0 / 4.0,
+            6.0 / 4.0,
+            8.0 / 4.0,
+        ]
+    )
+
     # Parameters saving
-    path = "/proj/sdp/data/LETHE"
-    np.save(path + "/z_grid.npy", z_grid)
-    np.save(path + "/rprs_grid.npy", rprs_grid)
+    np.save("/proj/sdp/data/LETHE" + "/parameters/z_grid.npy", z_grid)
+    np.save("/proj/sdp/data/LETHE" + "/parameters/rprs_grid.npy", rprs_grid)
 
     # First set of integrals
     def f1(r, theta, alpha, z):
@@ -39,33 +55,57 @@ def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
         x = 1.0 - z**2 - 2.0 * z * r * np.cos(theta) - r**2
         return f1(r, theta, alpha, z) * np.log(np.sign(x) * x) / 2.0
 
-    # powers of the LD model
-    alpha_test_1 = np.array([1.0 / 4.0, 2.0 / 4.0, 3.0 / 4.0, 4.0 / 4.0])
-    alpha_test = np.concatenate((alpha_test_1, 2.0 * alpha_test_1))
-
     def compute_integral_1(z, rprs, alpha):
-        res, err = integrate.dblquad(
+        res = integrate.dblquad(
             f1,
             0.0,
             2.0 * np.pi,  # bounds for theta
             0.0,
             rprs,  # bounds for r
             args=(alpha, z),
-        )
+        )[0]
         return res
 
+    vectorize_integral_1 = np.vectorize(compute_integral_1)
+
     def compute_integral_2(z, rprs, alpha):
-        res, err = integrate.dblquad(
+        res = integrate.dblquad(
             f2,
             0.0,
             2.0 * np.pi,  # bounds for theta
             0.0,
             rprs,  # bounds for r
             args=(alpha, z),
-        )
+        )[0]
         return res
 
-    interp_names = [
+    vectorize_integral_2 = np.vectorize(compute_integral_2)
+
+    # integrals computation
+    integral_1 = vectorize_integral_1(
+        z_grid[:, None, None],
+        rprs_grid[None, :, None],
+        alpha_grid[None, None, :4],
+    )
+
+    integral_2 = vectorize_integral_2(
+        z_grid[:, None, None],
+        rprs_grid[None, :, None],
+        alpha_grid[None, None, 4:],
+    )
+
+    integrals_list = [
+        integral_1[:, :, 0],
+        integral_1[:, :, 1],
+        integral_1[:, :, 2],
+        integral_1[:, :, 3],
+        integral_2[:, :, 0],
+        integral_2[:, :, 1],
+        integral_2[:, :, 2],
+        integral_2[:, :, 3],
+    ]
+
+    interpolation_names = [
         '/interpolator_G/f_G_0_25.pkl',
         '/interpolator_G/f_G_0_50.pkl',
         '/interpolator_G/f_G_0_75.pkl',
@@ -76,38 +116,11 @@ def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
         '/interpolator_F/f_F_2_00.pkl',
     ]
 
-    z, rprs = np.meshgrid(z_grid, rprs_grid, indexing='ij')
-    # Computation of the values
-    for index, name in enumerate(interp_names):
-        if index <= 3:
-            # Integrals computation
-            vec_integral_1 = np.vectorize(
-                lambda z_grid, rprs_grid: compute_integral_1(
-                    z_grid, rprs_grid, alpha_test[index]
-                )
-            )
-            integral = vec_integral_1(z, rprs)
-            np.save(
-                path + "/grid_G/" + str(alpha_test[index]) + ".npy", integral
-            )
+    interpolators_list = []
+    for integral in integrals_list:
+        interpolator = RectBivariateSpline(z_grid, rprs_grid, integral)
+        interpolators_list.append(interpolator)
 
-            # Interpolation computation
-            interpolator = RectBivariateSpline(z_grid, rprs_grid, integral)
-            with open(path + name, "wb") as file:
-                pickle.dump(interpolator, file)
-        else:
-            # Integrals computation
-            vec_integral_2 = np.vectorize(
-                lambda z_grid, rprs_grid: compute_integral_2(
-                    z_grid, rprs_grid, alpha_test[index]
-                )
-            )
-            integral = vec_integral_2(z, rprs)
-            np.save(
-                path + "/grid_F/" + str(alpha_test[index]) + ".npy", integral
-            )
-
-            # Interpolation computation
-            interpolator = RectBivariateSpline(z_grid, rprs_grid, integral)
-            with open(path + name, "wb") as file:
-                pickle.dump(interpolator, file)
+    for name, interpolator in zip(interpolation_names, interpolators_list):
+        with open("/proj/sdp/data/LETHE" + name, "wb") as file:
+            pickle.dump(interpolator, file)
