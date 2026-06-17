5171415e1a1e27c134f0d2bd86930cd2594740d7
Constant Bernardin <bernardin.constant7@gmail.com>
2026-06-17 09:43:22 -0700

    pull request correction

--- excalibur/transit/lethe.py
+++ excalibur/transit/lethe.py
@@ -5,6 +5,67 @@ from scipy import integrate
 from scipy.interpolate import RectBivariateSpline
 import pickle
 
+path = "/proj/sdp/data/LETHE"
+interpolation_names = [
+    '/interpolator_G/f_G_0_25.pkl',
+    '/interpolator_G/f_G_0_50.pkl',
+    '/interpolator_G/f_G_0_75.pkl',
+    '/interpolator_G/f_G_1_00.pkl',
+    '/interpolator_F/f_F_0_50.pkl',
+    '/interpolator_F/f_F_1_00.pkl',
+    '/interpolator_F/f_F_1_50.pkl',
+    '/interpolator_F/f_F_2_00.pkl',
+]
+
+
+# First set of integrals
+def f1(r, theta, alpha, z):
+    return (
+        np.pow(
+            np.max([0.0, 1.0 - z**2 - 2.0 * z * r * np.cos(theta) - r**2]),
+            alpha,
+        )
+        * r
+    )
+
+
+# Second set of integrals
+def f2(r, theta, alpha, z):
+    x = 1.0 - z**2 - 2.0 * z * r * np.cos(theta) - r**2
+    return f1(r, theta, alpha, z) * np.log(np.sign(x) * x) / 2.0
+
+
+# First integrals computation
+def compute_integral_1(z, rprs, alpha):
+    res = integrate.dblquad(
+        f1,
+        0.0,
+        2.0 * np.pi,  # bounds for theta
+        0.0,
+        rprs,  # bounds for r
+        args=(alpha, z),
+    )[0]
+    return res
+
+
+vectorize_integral_1 = np.vectorize(compute_integral_1)
+
+
+# Second integrals computation
+def compute_integral_2(z, rprs, alpha):
+    res = integrate.dblquad(
+        f2,
+        0.0,
+        2.0 * np.pi,  # bounds for theta
+        0.0,
+        rprs,  # bounds for r
+        args=(alpha, z),
+    )[0]
+    return res
+
+
+vectorize_integral_2 = np.vectorize(compute_integral_2)
+
 
 def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
     '''
@@ -37,49 +98,8 @@ def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
     )
 
     # Parameters saving
-    np.save("/proj/sdp/data/LETHE" + "/parameters/z_grid.npy", z_grid)
-    np.save("/proj/sdp/data/LETHE" + "/parameters/rprs_grid.npy", rprs_grid)
-
-    # First set of integrals
-    def f1(r, theta, alpha, z):
-        return (
-            np.pow(
-                np.max([0.0, 1.0 - z**2 - 2.0 * z * r * np.cos(theta) - r**2]),
-                alpha,
-            )
-            * r
-        )
-
-    # Second set of integrals
-    def f2(r, theta, alpha, z):
-        x = 1.0 - z**2 - 2.0 * z * r * np.cos(theta) - r**2
-        return f1(r, theta, alpha, z) * np.log(np.sign(x) * x) / 2.0
-
-    def compute_integral_1(z, rprs, alpha):
-        res = integrate.dblquad(
-            f1,
-            0.0,
-            2.0 * np.pi,  # bounds for theta
-            0.0,
-            rprs,  # bounds for r
-            args=(alpha, z),
-        )[0]
-        return res
-
-    vectorize_integral_1 = np.vectorize(compute_integral_1)
-
-    def compute_integral_2(z, rprs, alpha):
-        res = integrate.dblquad(
-            f2,
-            0.0,
-            2.0 * np.pi,  # bounds for theta
-            0.0,
-            rprs,  # bounds for r
-            args=(alpha, z),
-        )[0]
-        return res
-
-    vectorize_integral_2 = np.vectorize(compute_integral_2)
+    np.save(path + "/parameters/z_grid.npy", z_grid)
+    np.save(path + "/parameters/rprs_grid.npy", rprs_grid)
 
     # integrals computation
     integral_1 = vectorize_integral_1(
@@ -105,22 +125,11 @@ def grid_generation(step_z_1=0.01, step_z_2=0.001, step_rprs=0.002):
         integral_2[:, :, 3],
     ]
 
-    interpolation_names = [
-        '/interpolator_G/f_G_0_25.pkl',
-        '/interpolator_G/f_G_0_50.pkl',
-        '/interpolator_G/f_G_0_75.pkl',
-        '/interpolator_G/f_G_1_00.pkl',
-        '/interpolator_F/f_F_0_50.pkl',
-        '/interpolator_F/f_F_1_00.pkl',
-        '/interpolator_F/f_F_1_50.pkl',
-        '/interpolator_F/f_F_2_00.pkl',
-    ]
-
     interpolators_list = []
     for integral in integrals_list:
         interpolator = RectBivariateSpline(z_grid, rprs_grid, integral)
         interpolators_list.append(interpolator)
 
     for name, interpolator in zip(interpolation_names, interpolators_list):
-        with open("/proj/sdp/data/LETHE" + name, "wb") as file:
+        with open(path + name, "wb") as file:
             pickle.dump(interpolator, file)
