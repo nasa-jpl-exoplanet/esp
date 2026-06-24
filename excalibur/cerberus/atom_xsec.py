088703a88dd9bc77cca5b819a790f3bb79d25350
Constant Bernardin <bernardin.constant7@gmail.com>
2026-06-24 11:06:54 -0700

    corrections_v2

--- excalibur/cerberus/atom_xsec.py
+++ excalibur/cerberus/atom_xsec.py
@@ -5,7 +5,7 @@ import numpy as np
 import json
 import warnings
 
-from scipy.special import wofz
+from scipy import special
 from scipy.interpolate import interp1d
 from scipy.constants import c, h, k, m_e, physical_constants, eV
 
@@ -15,7 +15,7 @@ TREF = 296e0
 PREF = 1e0
 
 # physical constants
-ecgs = 4.80320427e-10  # Electron charge in cgs units (statC)
+charge_cgs = 4.80320427e-10  # Electron charge in cgs units (statC)
 c_cgs = 1e2 * c  # Speed of light in cgs units (cm)
 me_cgs = 1e3 * m_e  # Electron mass in cgs units (g)
 c2 = h * c_cgs / k
@@ -72,8 +72,10 @@ def load_part_func(specie):
 # -- COLISIONNAL BROADENING COEFFICIENTS COMPUTATION -- --------------
 def gammavld(gamma_vdw, m_s, broadener):
 
-    alphaH = 0.666793  # Polarisability of atomic hydrogen (A^-3)
-    mH = 1.007825  # Mass of atomic hydrogen (u)
+    alphah = 0.666793  # Polarisability of atomic hydrogen (A^-3)
+    mh = 1.007825  # Mass of atomic hydrogen (u)
+    mp = 0.0
+    alphap = 0.0
 
     if broadener == 'H2':
         alphap = 0.805000  # Polarisability of molecular hydrogen (A^-3)
@@ -84,38 +86,33 @@ def gammavld(gamma_vdw, m_s, broadener):
         mp = 4.002603  # Mass of helium (u)
 
     # Compute Lorentzian HWHM
-    gammaL0 = (
+    gammal0 = (
         2.2593427e7
         * gamma_vdw
-        * np.power(((mH * (m_s + mp)) / (mp * (m_s + mH))), (3e0 / 1e1))
-        * np.power((alphap / alphaH), (2e0 / 5e0))
+        * np.power(((mh * (m_s + mp)) / (mp * (m_s + mh))), (3e0 / 1e1))
+        * np.power((alphap / alphah), (2e0 / 5e0))
     )
 
-    # Temperature exponent
-    nL = 7e-1
-
-    return gammaL0, nL
+    return gammal0
 
 
 # --------- ----------------------------------------------------------
 # -- COLISIONNAL BROADENING COMPUTATION -- ---------------------------
-def H2Hebroadening(
-    gamma0H2,
+def h2hebroadening(
+    gamma0h2,
     T,
-    nLH2,
     pressure,
-    XH2,
-    gamma0He,
-    nLHe,
+    xh2,
+    gamma0he,
 ):
-    gamma = gamma0H2 * np.power((TREF / T), nLH2) * (
+    gamma = gamma0h2 * np.power((TREF / T), 7e-1) * (
         pressure / PREF
-    ) * XH2 + gamma0He * np.power(  # H2+He Lorentzian HWHM for given T, pressure, and J (ang. mom.)
-        (TREF / T), nLHe
+    ) * xh2 + gamma0he * np.power(  # H2+He Lorentzian HWHM for given T, pressure, and J (ang. mom.)
+        (TREF / T), 7e-1
     ) * (
         pressure / PREF
     ) * (
-        1e0 - XH2
+        1e0 - xh2
     )
 
     return gamma
@@ -125,28 +122,25 @@ def H2Hebroadening(
 # -- PARTITION FUNCTION COMPUTATION -- -------------------------------
 def partition_function(specie, T):
     temp, Q = load_part_func(specie)
-    Tmin, Tmax = np.min(temp), np.max(temp)
+    temp_min, temp_max = np.min(temp), np.max(temp)
     f = interp1d(temp, Q, kind='linear', fill_value="extrapolate")
 
-    def Qwarn(T):
-        if np.any((T < Tmin) | (T > Tmax)):
-            warnings.warn(
-                f"Extrapolation used for T = {T}. "
-                f"Valid range is [{Tmin}, {Tmax}]",
-                RuntimeWarning,
-            )
-        return f(T)
-
-    return Qwarn(T)
+    if np.any((T < temp_min) | (T > temp_max)):
+        warnings.warn(
+            f"Extrapolation used for T = {T}. "
+            f"Valid range is [{temp_min}, {temp_max}]",
+            RuntimeWarning,
+        )
+    return f(T)
 
 
 # --------- ----------------------------------------------------------
 # -- LINE INTENSITY COMPUTATION -- -----------------------------------
-def Svald(gf, Elow, nu0, T, Q):
+def vald_intensity(gf, elow, nu0, T, Q):
     S = (
-        ((gf * np.pi * ecgs**2) / (me_cgs * c_cgs**2))
+        ((gf * np.pi * charge_cgs**2) / (me_cgs * c_cgs**2))
         * (1e0 / Q)
-        * np.exp(-1e0 * c2 * Elow / T)
+        * np.exp(-1e0 * c2 * elow / T)
         * (1e0 - np.exp(-1e0 * c2 * nu0 / T))
     )
     return S
@@ -154,7 +148,7 @@ def Svald(gf, Elow, nu0, T, Q):
 
 # --------- ----------------------------------------------------------
 # -- VOIGT PROFILE COMPUTATION -- ------------------------------------
-def Voigt(nu_grid, nu0, T, mass, gamma):
+def voigt(nu_grid, nu0, T, mass, gamma):
     alpha = np.sqrt(2e0 * k * T * np.log(2e0) / mass) * nu0 / c
     x = np.sqrt(np.log(2e0)) * (np.outer(1.0 / alpha, (nu_grid - nu0)))
     y = np.sqrt(np.log(2e0)) * (np.outer(gamma / alpha, np.ones(len(nu_grid))))
@@ -162,42 +156,55 @@ def Voigt(nu_grid, nu0, T, mass, gamma):
     coeff = np.sqrt(np.log(2e0) / np.pi) * np.outer(
         1e0 / alpha, np.ones(len(nu_grid))
     )
-    return coeff * np.real(wofz(z))
+    return coeff * np.real(special.wofz(z))
 
 
 # --------- ----------------------------------------------------------
 # -- CROSS SECTION COMPUTATION FOR 1 LINE-- --------------------------
-def single_line_sigma(w_grid, specie, line, Q, T, pressure, XH2):
+def single_line_sigma(w_grid, specie, line, Q, parameters):
 
-    # line infomations
-    nu0 = 1.0 / line["WL_vacuum"] * 1e4  # conversion from micron to cm-1
-    nu_grid = 1.0 / w_grid * 1e4  # conversion from micron to cm-1
-    gf = line["gf"]
-    Elow = (
-        line["E_low"] * eV / h / c_cgs
-    )  # conversion from eV to cm-1 for the computation of the line intensity
-    gamma_nat = line["Rad"]
-    gamma_vdw = line["Waals"]
+    nu_grid = 1e0 / w_grid * 1e4  # conversion from micron to cm-1
 
     # line intensity computation
-    S = Svald(gf, Elow, nu0, T, Q)
+    S = vald_intensity(
+        line["gf"],
+        line["E_low"] * eV / h / c_cgs,
+        1e0 / line["WL_vacuum"] * 1e4,
+        parameters[:, 0],
+        Q,
+    )
+
+    # atomic mass
+    mass = ATOMIC_MASS[specie] * amu
 
     # broadening
-    gamma0H2, nLH2 = gammavld(gamma_vdw, ATOMIC_MASS[specie], 'H2')
-    gamma0He, nLHe = gammavld(gamma_vdw, ATOMIC_MASS[specie], 'He')
-    gamma_brd = H2Hebroadening(
-        gamma0H2, T, nLH2, pressure, XH2, gamma_0_He, nLHe
+    gamma0h2 = gammavld(line["Waals"], mass / amu, 'H2')
+    gamma0he = gammavld(line["Waals"], mass / amu, 'He')
+    gamma = h2hebroadening(
+        gamma0h2,
+        parameters[:, 0],
+        parameters[:, 1],
+        parameters[:, 2],
+        gamma_0_he,
     )
-    gamma = gamma_brd + 1e0 / (4e0 * np.pi * (c_cgs)) * gamma_nat
+    gamma = gamma + 1e0 / (4e0 * np.pi * (c_cgs)) * line["Rad"]
 
     # line cutoff
     wing_cutoff = 3e1
 
     # voigt profile construction
-    idx = np.where(np.abs(nu_grid - nu0) <= wing_cutoff)[0]
-    sigma = np.zeros((len(T), len(nu_grid)))
+    idx = np.where(
+        np.abs(nu_grid - 1e0 / line["WL_vacuum"] * 1e4) <= wing_cutoff
+    )[0]
+    sigma = np.zeros((len(parameters[:, 0]), len(nu_grid)))
     if len(idx):
-        phi = Voigt(nu_grid[idx], nu0, T, mass, gamma)
+        phi = voigt(
+            nu_grid[idx],
+            1e0 / line["WL_vacuum"] * 1e4,
+            parameters[:, 0],
+            mass,
+            gamma,
+        )
         sigma[:, idx] = S[:, None] * phi
 
     return sigma
@@ -205,51 +212,41 @@ def single_line_sigma(w_grid, specie, line, Q, T, pressure, XH2):
 
 # --------- ----------------------------------------------------------
 # -- CROSS SECTION COMPUTATION WITH LINE BY LINE -- ------------------
-def atom_xsec(
-    w_grid,
-    specie,
-    T,
-    pressure,
-    XH2,
-):
+def atom_xsec(w_grid, specie, parameters):
     # partition function computation
     Q = partition_function(specie, T)
 
     # lines loading
     data = load_lines()
 
-    sigma_total = np.zeros((len(T), len(wavelength_grid)))
+    sigma_total = np.zeros((len(T), len(w_grid)))
     for line in data[specie].values():
-        sigma_total += single_line_sigma(
-            w_grid, specie, line, Q, T, pressure, XH2
-        )
+        sigma_total += single_line_sigma(w_grid, specie, line, Q, parameters)
     return sigma_total
 
 
 # --------- ----------------------------------------------------------
 # -- GRID STORING IN /proj/sdp/data/CERBERUS/ATOM_XSEC -- ------------
-def grid_generation(elem, temp, pressure, XH2, wgrid):
+def grid_generation(elem, temp, pressure, xh2, wgrid):
 
-    Tgrid, Pgrid, XH2grid = np.meshgrid(temp, pressure, XH2, indexing='ij')
-
-    Tcomb = T_grid.flatten()
-    Pcomb = P_grid.flatten()
-    XH2comb = XH2grid.flatten()
-
-    grid = atom_xsec(
-        w_grid=wgrid,
-        specie=elem,
-        T=Tcomb,
-        pressure=Pcomb,
-        XH2=XH2comb,
+    temp_grid, pressure_grid, xh2_grid = np.meshgrid(
+        temp, pressure, xh2, indexing='ij'
     )
+
+    temp_comb = temp_grid.flatten()
+    pressure_comb = pressure_grid.flatten()
+    xh2_comb = xh2_grid.flatten()
+
+    parameters = np.column_stack((temp_comb, pressure_comb, xh2_comb))
+
+    grid = atom_xsec(w_grid=wgrid, specie=elem, parameters=parameters)
 
     np.save("/proj/sdp/data/CERBERUS/ATOM_XSEC/wgrid.npy", wgrid)
     np.save("/proj/sdp/data/CERBERUS/ATOM_XSEC/temp.npy", temp)
     np.save("/proj/sdp/data/CERBERUS/ATOM_XSEC/pressure.npy", pressure)
-    np.save("/proj/sdp/data/CERBERUS/ATOM_XSEC/X_H2.npy", pressure)
+    np.save("/proj/sdp/data/CERBERUS/ATOM_XSEC/X_H2.npy", xh2)
 
-    grid_4d = grid.reshape((len(temp), len(pressure), len(XH2), -1))
+    grid_4d = grid.reshape((len(temp), len(pressure), len(xh2), -1))
 
     np.save(
         "/proj/sdp/data/CERBERUS/ATOM_XSEC/" + elem + "/grid_4d.npy", grid_4d
