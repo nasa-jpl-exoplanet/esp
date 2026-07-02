ac2abb8d98b09e46e560e2ee528f59f233ba6608
bryden <bryden@jpl.nasa.gov>
2026-07-02 10:06:00 -0700

    switch from Luke T-P profile to simple T grid

--- excalibur/util/cerberus.py
+++ excalibur/util/cerberus.py
@@ -23,6 +23,7 @@ log = logging.getLogger(__name__)
 
 
 def calcTEA(
+    temperatures,
     tp_coeffs,
     pressure,
     species=None,
@@ -169,7 +170,9 @@ def calcTEA(
         return abund
 
     pressure = np.asarray(pressure, dtype=float)
-    temperature = _make_tp_profile(pressure, *tp_coeffs)
+    # temperature = _make_tp_profile(pressure, *tp_coeffs)
+    # T-P profile is no longer calculated here; full T array is passed in
+    temperature = temperatures
     if plot_tp:
         plt.plot(temperature, pressure)
         plt.yscale("log")
