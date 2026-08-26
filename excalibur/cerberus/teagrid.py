ab413377fb4d9958ddf10b8fb2999e9d6f9582e6
bryden <bryden@jpl.nasa.gov>
2026-08-26 13:06:11 -0700

    constant TEA grid maker (from PR#364)

--- excalibur/cerberus/teagrid.py
+++ excalibur/cerberus/teagrid.py
@@ -6,6 +6,7 @@
 import os
 import numpy as np
 import excalibur
+from excalibur.util.cerberus import calcTEA
 from scipy.interpolate import RegularGridInterpolator
 
 INTERP_TEA_DIR = os.path.join(
@@ -63,3 +64,72 @@ def get_TEA_grid():
         )
 
     return interp_tea
+
+
+def grid_generation(parameters, species, verbose=False):
+    # Parameter 1 : temperature in K
+    # Parameter 2 : pressure in bar
+    # Parameter 3 : metallicities, log10 values
+    # Parameter 4 : CtoOs, log10 values
+    # Species : list of species with the names from TEA
+    # ex : ['CH4_g, N2_ref, ...]
+
+    # parameters saving
+    np.save(
+        INTERP_TEA_DIR + 'grid_parameters/temperature.npy', parameters[:, 0]
+    )
+    np.save(INTERP_TEA_DIR + 'grid_parameters/pressure.npy', parameters[:, 1])
+    np.save(INTERP_TEA_DIR + 'grid_parameters/XtoH.npy', parameters[:, 2])
+    np.save(INTERP_TEA_DIR + 'grid_parameters/CtoO.npy', parameters[:, 3])
+
+    temp_grid, pressure_grid = np.meshgrid(
+        parameters[:, 0], parameters[:, 1], indexing='ij'
+    )
+    temp_comb = temp_grid.flatten()
+    pressure_comb = pressure_grid.flatten()
+
+    # species names for calling the cross sections
+    # TIO for the cross section library
+    species_name = [
+        'TIO' if el == 'TiO_g' else el.split('_')[0] for el in species
+    ]
+
+    # dictionary creation
+    dic = {
+        molecule: np.zeros(
+            (
+                len(parameters[:, 0]),
+                len(parameters[:, 1]),
+                len(parameters[:, 2]),
+                len(parameters[:, 3]),
+            )
+        )
+        for molecule in species_name
+    }
+
+    # call TEA for each XtoH and CtoO
+    for i, XtoH in enumerate(parameters[:, 2]):
+        for j, CtoO in enumerate(parameters[:, 3]):
+            if verbose:
+                # to allow to know at what steps we are for the computation
+                print("metallicity :", 10.0**XtoH)
+                print("[C/O] :", 10.0**CtoO)
+            # TEA computation for all the temperatures and pressures
+            mixratioprofiles = calcTEA(
+                temp_comb,
+                pressure_comb,
+                species,
+                metallicity=10.0**XtoH,
+                C_O=0.55 * 10.0**CtoO,
+            )
+            for molecule in mixratioprofiles:
+                dic[molecule][:, :, i, j] = np.reshape(
+                    mixratioprofiles[molecule],
+                    ((len(parameters[:, 0]), len(parameters[:, 1]))),
+                )
+
+    # grid saving for each molecule
+    for molecule in dic:
+        np.save(INTERP_TEA_DIR + molecule + '.npy', dic[molecule])
+
+    return
