0e13abeae2cacf34175735db887f4aef7a240b5f
Geoffrey Bryden <gbryden@users.noreply.github.com>
2026-09-08 10:57:42 -0700

    Teagrid cleanup cleanup (#378)
    * store TEAgrid under a subdirectory modelName
    
    * TEAgrid with modelName, species save, permissions set, etc
    
    * store TEAgrid under a subdirectory modelName
    
    * remove absorb(), the original HITRAN routine
    
    * oops. absorb() added back in; used in xslib
    
    * lint
    
    * unused param
    
    * lint shame
    
    * remove qtgrid from atmos FM; keep it in xslib/absorb()
    
    * remove qtgrid
    
    * pull out atom xsec calc as getatomxm()
    
    * setting read-write permissions not allowed, it seems
    
    * oops
    
    * whitespace
    
    * allow for selection of molecules within atmos() spectrum (different from xslib selection)
    
    * lint
    
    * split fitCloudProperties into CTP and Haze options
    
    * edit fixed param passing for fitCTP/fitHaze cases
    
    * remove STIS/WFC-offset forward models
    
    * one more related edit
    
    * lint
    
    * lint
    
    * lint
    
    * oops
    
    * oops
    
    * main merge
    
    * syntax problem

--- excalibur/cerberus/forward_model.py
+++ excalibur/cerberus/forward_model.py
@@ -658,7 +658,7 @@ def gettau(
                     elem,
                 )
 
-        if sigma:
+        if sigma is not None:
             if True in (sigma < 0):
                 sigma[sigma < 0] = 0e0
                 pass
